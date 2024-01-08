cf673b3c0fbd531bf986862ccf3f8f4c3e94b804
slash2270 <slash2270@gmail.com>
2024-01-08 14:04:38 +0800

    ListView+GoRouter+MVVM+Animation

--- lib/view/child/child/button_view.dart
+++ lib/view/child/child/button_view.dart
@@ -2,25 +2,56 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'dart:math';
 
-class ButtonView extends StatelessWidget {
+class ButtonView extends StatefulWidget {
   final bool isItemChild;
   final bool? isClick;
   const ButtonView({Key? key, required this.isItemChild, this.isClick}) : super(key: key);
 
+  @override
+  State<ButtonView> createState() => _ButtonViewState();
+}
+
+class _ButtonViewState extends State<ButtonView> with TickerProviderStateMixin {
+
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(vsync: this, duration: const Duration(milliseconds: 500));
+    _animation = Tween<double>(begin: 0, end: pi / 5).animate(_animationController);
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(covariant ButtonView oldWidget) {
+    if (oldWidget.isClick != widget.isClick) {
+      widget.isClick! ? _animationController.forward() : _animationController.reverse();
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+
   Widget _rotateView({required double width, required double height}) {
-    return AnimatedRotation(
-      curve: Curves.linear,
-      turns: isClick! ? pi / 5 : 0,
-      duration: const Duration(milliseconds: 500),
-      child: Container(
-        width: width.w,
-        height: height.w,
-        decoration: BoxDecoration(
-          color: Colors.black,
-          borderRadius: BorderRadius.all(Radius.circular(20.w)),
+    return AnimatedBuilder(animation: _animation, builder: (context, setWidget){
+      return Transform.rotate(
+        angle: _animation.value,
+        child: Container(
+          width: width.w,
+          height: height.w,
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.all(Radius.circular(20.w)),
+          ),
         ),
-      ),
-    );
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
   @override
@@ -28,7 +59,7 @@ class ButtonView extends StatelessWidget {
     return SizedBox(
         height: 20.w,
         width: 20.w,
-        child: isItemChild
+        child: widget.isItemChild
             ? Icon(
           Icons.arrow_forward_ios_rounded,
           size: 20.w,
