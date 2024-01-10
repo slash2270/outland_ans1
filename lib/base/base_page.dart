12532cb4344e8b5d43cf11b753d27306bab13c29
slash2270 <slash2270@gmail.com>
2024-01-10 21:59:31 +0800

    ListView+GoRouter+MVVM+Animation

--- lib/base/base_page.dart
+++ lib/base/base_page.dart
@@ -1,6 +1,3 @@
-import 'dart:io';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:outland/base/child/base_sencond_page.dart';
